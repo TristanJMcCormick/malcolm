0c9f9e0f554e9d179740497a61bb9104e44eb3e7
Tristan McCormick <tristan@Rachels-MacBook-Air.local>
2023-01-29 18:17:51 -0500

    Update to bachelor party

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from "react";
-import malc from "./malcolm.jpg";
-import insta from "./instagram.png";
-import linkedin from "./linkedin.png";
+import goleta from "./goleta.jpeg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img alt="malc-profile" src={malc} style={{ height: "40vh" }} />
+        <img alt="goleta-profile" src={goleta} style={{ height: "40vh" }} />
       </header>
-      <h1>Hi there!</h1>
-      <p>This is Malcolm Kerr's budding website, stay tuned for updates.</p>
-      <div className="social-section">
-        <p>
-          <a
-            className="social-tile"
-            href="https://www.linkedin.com/in/malcolm-kerr/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img className="social-icon" alt="linked-in-icon" src={linkedin} />
-            LinkedIn
-          </a>
-        </p>
-        <p>
-          <a
-            className="social-tile"
-            href="https://www.instagram.com/malclorbkerr/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img className="social-icon" alt="instagram-icon" src={insta} />
-            Instagram
-          </a>
-        </p>
-      </div>
+      <h1>GOLETA</h1>
+      <h1>April 2023</h1>
+      <h1>YOU</h1>
+      <h1>YOUR BOYS</h1>
+      <h1>START STRETCHING</h1>
     </div>
   );
 }
